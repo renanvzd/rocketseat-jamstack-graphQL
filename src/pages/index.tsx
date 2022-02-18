55581c870d9b6e0e6d3f36dd2e8b62ba5a6186c1
Renan Veronez Drechsler <renanvz@outlook.com>
2022-02-17 22:33:17 -0300

    getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { GetServerSideProps } from "next";
 import { usePageQuery, PageDocument } from "../generated/graphql"
+import { client, ssrCache } from "../lib/urql"
 
 export default function Home() {
   const [{ data }] = usePageQuery({
@@ -12,4 +14,14 @@ export default function Home() {
       <h1>{data?.page?.title}</h1>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: 'home' }).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData()
+    }
+  }
 }
