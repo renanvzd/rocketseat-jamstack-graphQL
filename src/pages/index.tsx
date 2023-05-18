9f389fa680e5c37b7d66fac0a59c02e82c400353
Renan Veronez Drechsler <renanvz@outlook.com>
2023-05-18 20:18:58 +0100

    Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,26 @@
 import { GetServerSideProps } from "next";
-import { usePageQuery, PageDocument } from "../generated/graphql"
-import { client, ssrCache } from "../lib/urql"
+import { Features } from "../components/Features";
+import { Hero } from "../components/Hero";
+import { Pricing } from "../components/Pricing";
+import { usePageQuery, PageDocument } from "../generated/graphql";
+import { client, ssrCache } from "../lib/urql";
 
 export default function Home() {
-  const [{ data }] = usePageQuery({
-    variables: {
-      slug: 'post-one',
-    }
-  })
-
+  const [{ data }] = usePageQuery({ variables: { slug: "home" } });
   return (
     <>
-      <h1>{data?.page?.title}</h1>
+      <Hero title={data?.page?.title} subtitle={data?.page?.subtitle} />
+      <Features />
+      <Pricing />
     </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  await client.query(PageDocument, { slug: 'post-one' }).toPromise();
-
+  await client.query(PageDocument, { slug: "home" }).toPromise();
   return {
     props: {
-      urqlState: ssrCache.extractData()
-    }
-  }
-}
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
